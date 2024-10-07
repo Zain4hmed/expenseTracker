522262164f3bc668168196e5498aec018827683d
ZainAhmed08 <zainahmed01997@gmail.com>
2024-10-08 00:17:41 +0530

    added neccessary front end stuff

--- src/main/java/com/zain/expenseTracker/controller/FrontendController.java
+++ src/main/java/com/zain/expenseTracker/controller/FrontendController.java
@@ -1,4 +1,13 @@
 package com.zain.expenseTracker.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FrontendController {
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/index.html";  // Redirect to static file
+    }
 }
